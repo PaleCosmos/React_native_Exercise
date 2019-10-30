6ab0fbd4f8d856f53d5de463e4c5193a79ccdab2
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-10-30 17:22:15 +0900

    good

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>개신기해  ㅇㄴㄹ  ㅇ!</Text>
+      <View style={styles.redView}>
+        <Text style={styles.text}>이것은 빨강색</Text>
+      </View>
+      <View style={styles.greenView} >
+      <Text style={styles.text}>이것은 초록색</Text>
+      </View>
+      <View style={styles.blueView} >
+      <Text style={styles.text}>이것은 파랑색</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +20,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ccfafd',
     justifyContent: 'center',
   },
+
+  text: {
+    color: "white",
+    textAlign:"center",
+    alignContent: "center",
+    fontSize: 20
+  },
+
+  redView: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign:"center",
+    backgroundColor: "red"
+  },
+  greenView: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign:"center",
+    backgroundColor: "green"
+  },
+  blueView: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign:"center",
+    backgroundColor: "blue"
+  }
 });
