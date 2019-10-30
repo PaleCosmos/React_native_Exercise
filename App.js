8b79811f7868df923a6775cced06b5240d13ea27
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-10-30 17:35:34 +0900

    Loading.js

--- App.js
+++ App.js
@@ -1,52 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.redView}>
-        <Text style={styles.text}>이것은 빨강색</Text>
-      </View>
-      <View style={styles.greenView} >
-      <Text style={styles.text}>이것은 초록색</Text>
-      </View>
-      <View style={styles.blueView} >
-      <Text style={styles.text}>이것은 파랑색</Text>
-      </View>
-    </View>
+    <Loading/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccfafd',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: "white",
-    textAlign:"center",
-    alignContent: "center",
-    fontSize: 20
-  },
-
-  redView: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign:"center",
-    backgroundColor: "red"
-  },
-  greenView: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign:"center",
-    backgroundColor: "green"
-  },
-  blueView: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign:"center",
-    backgroundColor: "blue"
-  }
-});
