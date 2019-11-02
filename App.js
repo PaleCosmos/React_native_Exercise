81b0a86dfd3defd35f84b969ca9441d489942828
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-11-03 00:51:56 +0900

    add Loading

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import Loading from "./Loading"
+import * as Location from "expo-location"
+import { Alert } from "react-native"
 
-export default function App() {
-  return (
-    <Loading/>
-  );
+export default class App extends React.Component {
+
+  state = {
+    isLoading: true
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      //this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
